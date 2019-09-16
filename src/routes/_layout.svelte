3c909f53ed288d38da95ccffbdba3877f45d1959
Mateiadrielrafael <rafaeladriel11@gmail.com>
2019-09-16 23:27:07 +0300

    added more meta tags

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -11,6 +11,12 @@
   // the contexts are then updated by the CanvasStack component
   const painting = new Painting(currentBrush$, brushes)
 
+  const title = "Visual studio paint"
+  const description = "Paint clone made in 1 week."
+  const defaultUrl = "https://visual-studio-paint.herokuapp.com"
+  const url = process.browser ? window.location.href : defaultUrl
+  const logo = "favicon.png"
+
   // current way of doing things
   setContext("painting", painting)
 </script>
@@ -26,6 +32,13 @@
   <link
     href="https://fonts.googleapis.com/icon?family=Material+Icons"
     rel="stylesheet" />
+
+  <meta name="Description" content={description} />
+
+  <meta property="og:title" content={title} />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content={url} />
+  <meta property="og:image" content={`${defaultUrl}/${logo}`} />
 
   <title>Visual studio paint</title>
 </svelte:head>
